6325e964addf789bfc4761cfb63dc8a0d9e5c9f6
greenLED <juan.mejias@reduc.edu.cu>
2015-12-17 23:09:09 -0500

    Agregado formulario inicial para agregar ficheros
    Aún no se guardan los ficheros, sólo se reciben y se indica su nombre.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,11 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+import views.html.index;
+import views.html.uploadForm;
+import views.html.uploadResult;
 
 public class Application extends Controller {
 
@@ -11,4 +13,18 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+	public Result uploadForm() {
+		return ok(uploadForm.render());
+	}
+
+	public Result upload() {
+		Http.MultipartFormData body = request().body().asMultipartFormData();
+		Http.MultipartFormData.FilePart file = body.getFile("file");
+		if (file != null) {
+			String fileName = file.getFilename();
+            return ok(uploadResult.render(fileName));
+        } else {
+			return badRequest();
+		}
+	}
 }
