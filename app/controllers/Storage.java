32873b9f68f03594f3d9d1357be7afd73233e6e6
greenLED <juan.mejias@reduc.edu.cu>
2016-01-06 11:53:08 -0500

    Integrados controlador y módulo de gestión de ficheros
    Aún no soporta eliminación de ficheros

--- app/controllers/Storage.java
+++ app/controllers/Storage.java
@@ -1,24 +1,38 @@
 package controllers;
 
+import com.google.inject.Inject;
+import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.mvc.Results;
 import views.html.uploadForm;
 import views.html.uploadResult;
 
+import java.nio.file.Path;
+import java.util.UUID;
+
 public class Storage extends Controller {
+    @Inject
+    public modules.storage.Storage storage;
+
     public Result uploadForm() {
         return ok(uploadForm.render());
     }
 
-    public Result upload() {
+    public F.Promise<Result> upload() {
         Http.MultipartFormData body = request().body().asMultipartFormData();
         Http.MultipartFormData.FilePart file = body.getFile("file");
         if (file != null) {
             String fileName = file.getFilename();
-            return ok(uploadResult.render(fileName));
+            String uuid = UUID.randomUUID().toString();
+            return storage.store(file.getFile().toPath(), uuid).map((F.Function<Void, Result>) aVoid -> ok(uploadResult.render(uuid)));
         } else {
-            return badRequest();
+            return F.Promise.promise(Results::badRequest);
         }
+    }
+
+    public F.Promise<Result> download(String id) {
+        return storage.retrieve(id).map((F.Function<Path, Result>) path -> ok(path.toFile()));
     }
 }
