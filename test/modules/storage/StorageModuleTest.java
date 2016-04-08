49be7295f639fd2292465dee001803d08b245de5
greenled <juan.mejias@reduc.edu.cu>
2016-04-08 15:34:08 -0400

    Added local storage module test again

---
+++ test/modules/storage/StorageModuleTest.java
@@ -0,0 +1,26 @@
+package modules.storage;
+
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.test.WithApplication;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static play.test.Helpers.running;
+
+public class StorageModuleTest extends WithApplication {
+    @Test
+    public void testLocalFilesystemStorageMapping () {
+        Application application = new GuiceApplicationBuilder()
+                .configure("storage.type", "local")
+                .build();
+
+        running (application, () -> {
+            Storage instance = application.injector().instanceOf(Storage.class);
+            assertThat("Storage has not been mapped", instance, notNullValue());
+            assertThat("Storage has been mapped to a wrong type", instance instanceof LocalFilesystemStorage, is(true));
+        });
+    }
+}
