148d3b482998032a267ac13d495d4c894bf22485
greenLED <juan.mejias@reduc.edu.cu>
2016-05-15 09:48:16 -0400

    Added some comments to scripts.js file

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,4 @@
-// Prepares upload modal
+// Resets upload modal
 function resetUploadModal () {
     $('.upload-file-modal .upload-btn').button('reset');
     $('.upload-file-modal .cancel-btn').show();
@@ -13,6 +13,8 @@ function updateUploadProgressBarValue (percent) {
     $('.upload-progress .progress-bar .sr-only').html(percent + "% " + messages['completed']);
 }
 
+// Gets current value of search box's tags input, checks it is not in the upload tags list,
+// creates a tag from it and adds it to the search tags list
 function addTagFromSearchBoxTagsInput () {
     var tagsInput = $('.search-box .tags-input.typeahead.tt-input');
     if (tagsInput.val() != null && tagsInput.val() != "" && app.searchTags.findWhere({name: tagsInput.val()}) == null) {
@@ -22,6 +24,8 @@ function addTagFromSearchBoxTagsInput () {
     tagsInput.focus();
 }
 
+// Gets current value of upload form's tags input, checks it is not in the upload tags list,
+// creates a tag from it and adds it to the upload tags list
 function addTagFromUploadBoxTagsInput () {
     var tagsInput = $('.upload-file-modal .tags-input.typeahead.tt-input');
     if (tagsInput.val() != null && tagsInput.val() != "" && app.uploadTags.findWhere({name: tagsInput.val()}) == null) {
@@ -31,6 +35,7 @@ function addTagFromUploadBoxTagsInput () {
     tagsInput.focus();
 }
 
+// Applies a search filter by tags
 function applySearchFilter () {
     var tags = [];
     app.searchTags.each(function(tag) {
@@ -42,6 +47,7 @@ function applySearchFilter () {
 $(document).ready(function(){
     resetUploadModal();
 
+    // Source for typeaheads
     var tags = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -51,6 +57,7 @@ $(document).ready(function(){
         }
     });
 
+    // Initialize typeaheads
     $('.typeahead').typeahead({
         highlight: true
     },
@@ -60,19 +67,26 @@ $(document).ready(function(){
         source: tags
     });
 
+    // When a tag is selected from the dropdown of the upload form's tags input, add it to the list
     $('.upload-file-modal .tags-input').bind('typeahead:select', addTagFromUploadBoxTagsInput);
 
+    // When a tag is selected from the dropdown of the search box's tags input, add it to the list
     $('.search-box .tags-input').bind('typeahead:select', addTagFromSearchBoxTagsInput);
 
+    // When the add-tag-button next to the upload form's tags input is clicked, add a new tag to the list
     $('.upload-file-modal .add-tag-btn').click(addTagFromUploadBoxTagsInput);
 
+    // Reset the upload modal when it hides
     $('.upload-file-modal').on('hidden.bs.modal', resetUploadModal);
 
     $('.upload-form').submit(function( event ) {
+        // Prevent the upload form from being sent without tags
         event.preventDefault();
 
         // Get the data from the upload form
         var formData = new FormData($('.upload-form')[0]);
+
+        // Add tags
         app.uploadTags.each(function(tag) {
             formData.append('tags', tag.get("name"));
         });
@@ -107,15 +121,20 @@ $(document).ready(function(){
             processData: false
         });
 
+        // Hide the cancel button of the upload modal
         $('.upload-file-modal .cancel-btn').hide();
+
+        // Change upload button's state to "uploading"
         $('.upload-file-modal .upload-btn').button('uploading');
 
+        // Handle upload progress events
         function progressHandler(e){
             if(e.lengthComputable){
                 updateUploadProgressBarValue (e.loaded / e.total * 100);
             }
         }
 
+        // Handle upload complete event
         function completeHandler(){
             var searchTags = app.searchTags.pluck("name");
             var uploadTags = app.uploadTags.pluck("name");
@@ -128,6 +147,7 @@ $(document).ready(function(){
             resetUploadModal();
         }
 
+        // Handle upload error event
         function errorHandler(data){
             $('.upload-file-modal .alert').remove();
             var template = _.template($('#alert-template').html());
