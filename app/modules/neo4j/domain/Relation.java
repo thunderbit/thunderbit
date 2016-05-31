98ca0713a016a9fdbcf77cde23ba2a8116aa8f8a
greenLED <juan.mejias@reduc.edu.cu>
2016-05-30 20:13:53 -0400

    Added @Property to weight on Relation class

--- app/modules/neo4j/domain/Relation.java
+++ app/modules/neo4j/domain/Relation.java
@@ -18,6 +18,7 @@
 package modules.neo4j.domain;
 
 import org.neo4j.ogm.annotation.EndNode;
+import org.neo4j.ogm.annotation.Property;
 import org.neo4j.ogm.annotation.RelationshipEntity;
 import org.neo4j.ogm.annotation.StartNode;
 
@@ -29,6 +30,7 @@ public class Relation extends Entity {
     @EndNode
     private Tag eTag;
     
+    @Property
     private int weight;
     
     public Relation setNewRelation(Tag start, Tag end, int weight) {
